9f87cd60d4d77c144cb8702b825bf47a13ff28e3
Kenneth Daily <kmdaily@gmail.com>
2015-06-16 23:42:33 +0000

    update file readers, remove other ccle ge data

--- code/getData.R
+++ code/getData.R
@@ -1,45 +1,40 @@
 library(dplyr)
 library(tidyr)
 library(data.table)
+library(reshape2)
 library(synapseClient)
 synapseLogin()
 
-# CCLE Gene Expression
-ccleFullGELineNames <- c("MCF7_BREAST", "PC3_PROSTATE", "HPAC_PANCREAS", "A375_SKIN", "A549_LUNG")
-ccleFullGEObj <- synGet("syn2318265")
-ccleFullGE <- readRDS(getFileLocation(ccleFullGEObj))
-ccleFullGELINCS <- ccleFullGE[, ccleFullGELineNames]
+# CCLE
+ccleLineNames <- c("MCF7", "PC3", "HPAC", "A375", "A549")
 
 # CCLE Gene Expression (maybe processed?)
-ccleGELineNames <- c("MCF7", "PC3", "HPAC", "A375", "A549")
 ccleGEObj <- synGet("syn2293298")
 ccleGE <- fread(getFileLocation(ccleGEObj), data.table=FALSE)
-ccleGELINCS <- ccleGE[, ccleGELineNames]
+ccleGELINCS <- ccleGE[, ccleLineNames]
 
 # CCLE Copy Number
-ccleCNALineNames <- c("MCF7", "PC3", "HPAC", "A375", "A549")
 ccleCNAObj <- synGet("syn2293299")
 ccleCNA <- fread(getFileLocation(ccleCNAObj), data.table=FALSE)
-ccleCNALINCS <- ccleCNA[, ccleCNALineNames]
+ccleCNALINCS <- ccleCNA[, ccleLineNames]
 
 # CCLE Drug EC50
 # Data is cell lines (rows) x drugs (columns)
-ccleEC50LineNames <- c("MCF7", "PC3", "HPAC", "A375", "A549")
 ccleEC50Obj <- synGet("syn2293304")
-ccleEC50 <- fread(getFileLocation(ccleEC50Obj), data.table=FALSE)
-rownames(ccleEC50) <- ccleEC50$V1
-ccleEC50$V1 <- NULL
-ccleEC50LINCS <- t(ccleEC50[ccleEC50LineNames, ])
+ccleEC50LINCS <- fread(getFileLocation(ccleEC50Obj), data.table=FALSE) %>%
+  rename(cell_line_name=V1) %>% filter(cell_line_name %in% ccleLineNames) %>%
+  melt(id.vars='cell_line_name') %>%
+  dcast(variable ~ cell_line_name, value.var = 'value') %>%
+  rename(drug=variable)
 
 # CCLE Drug IC50
 # Data is cell lines (rows) x drugs (columns)
-ccleIC50LineNames <- c("MCF7", "PC3", "HPAC", "A375", "A549")
 ccleIC50Obj <- synGet("syn2293303")
-ccleIC50 <- fread(getFileLocation(ccleIC50Obj), data.table=FALSE)
-rownames(ccleIC50) <- ccleIC50$V1
-ccleIC50$V1 <- NULL
-ccleIC50LINCS <- t(ccleIC50[ccleIC50LineNames, ])
-
+ccleIC50LINCS <- fread(getFileLocation(ccleIC50Obj), data.table=FALSE) %>%
+  rename(cell_line_name=V1) %>% filter(cell_line_name %in% ccleLineNames) %>%
+  melt(id.vars='cell_line_name') %>%
+  dcast(variable ~ cell_line_name, value.var = 'value') %>%
+  rename(drug=variable)
 
 # Broad CTD2
 ctd2LineNames <- c("MCF7", "PC3", "HPAC", "A375", "A549")
@@ -48,8 +43,14 @@ ctd2LineNames <- c("MCF7", "PC3", "HPAC", "A375", "A549")
 ctd2AUCObj <- synGet("syn4260138")
 ctd2AUC <- fread(getFileLocation(ctd2AUCObj), data.table=FALSE)
 ctd2AUCLINCS <- ctd2AUC %>% filter(cell_line_name %in% ctd2LineNames)
+write.csv(ctd2AUCLINCS, file="CTD2_AUC_LINCS.csv", row.names=FALSE)
 
 # Raw viability
 ctd2RawViabilityObj <- synGet("syn4260136")
 ctd2RawViability <- fread(getFileLocation(ctd2RawViabilityObj), data.table=FALSE)
 ctd2RawViabilityLINCS <- ctd2RawViability %>% filter(cell_line_name %in% ctd2LineNames)
+
+# Avg pct viability
+ctd2PctViabilityObj <- synGet("syn4260137")
+ctd2PctViability <- fread(getFileLocation(ctd2PctViabilityObj), data.table=FALSE)
+ctd2PctViabilityLINCS <- ctd2PctViability %>% filter(cell_line_name %in% ctd2LineNames)
