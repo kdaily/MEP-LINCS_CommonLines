bd48673f6ee7f9b96a8730f98b45ad40ded1cf42
Kenneth Daily <kmdaily@gmail.com>
2015-07-15 17:43:17 +0000

    updated code

--- code/achilles.R
+++ code/achilles.R
@@ -6,17 +6,25 @@ library(synapseClient)
 library(rGithubClient)
 synapseLogin()
 
-# repo <- getRepo("kdaily/MEP-LINCS_CommonLines", ref="branch", refName="master")
-# thisScript <- getPermlink(repo, "code/achilles.R")
+repo <- getRepo("kdaily/MEP-LINCS_CommonLines", ref="branch", refName="master")
+thisScript <- getPermlink(repo, "code/achilles.R")
 
 projectId <- "syn4259323"
 proj <- synGet(projectId)
 
-availLines <- c() 
-
 achillesLineNames <- c("MCF7_BREAST", "A549_LUNG")
 
-id <- "syn2293298"
-fn <- "CCLE_GE_LINCS.csv"
-achillesGEObj <- synGet(id)
+id <- "syn4598414"
+fn <- "Achilles_Gs_LINCS.csv"
+achillesGsObj <- synGet(id)
 
+achillesGs <- fread(getFileLocation(achillesGsObj), skip=2, data.table=FALSE,
+                    select=c("Name", "Description", achillesLineNames)) %>%
+  rename(MCF7=MCF7_BREAST, A549=A549_LUNG)
+
+write.csv(achillesGs, file=fn)
+f <- File(fn, name="Achilles QC rnai Gs", parentId=proj@properties$id)
+synSetAnnotations(f) <- list(source="Achilles", dataType="rnai", fileType="genomicMatrix")
+act <- Activity(used=id, executed=thisScript)
+generatedBy(f) <- act
+f <- synStore(f)
