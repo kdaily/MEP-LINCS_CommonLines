06de7fde922a83aec80ef020f6c03e9ff3b00137
kdaily <kdaily@users.noreply.github.com>
2015-07-16 22:21:26 -0700

    cleanup

--- code/sangerCOSMICMutations.R
+++ code/sangerCOSMICMutations.R
@@ -1,27 +1,25 @@
-library(dplyr)
-library(tidyr)
-library(data.table)
-library(reshape2)
-library(synapseClient)
-library(rGithubClient)
-synapseLogin()
+# Getting the file needed from Synapse in this script requires Rsftp
+# devtools::install_github("Sage-Bionetworks/Rsftp")
 
-repo <- getRepo("kdaily/MEP-LINCS_CommonLines", ref="branch", refName="sangermut")
-thisScript <- getPermlink(repo, "code/sangerCOSMICMutations.R")
+library(dplyr)
+library(data.table)
+library(synapseClient)
+synapseLogin()
 
 projectId <- "syn4259323"
 proj <- synGet(projectId)
 
-# Sanger
 sangerLineNames <- c("MCF7", "PC3", "YAPC", "A375", "A549")
 
 # Must gunzip this file!
 # read.table cannot read it b/c of uneven rows, use fread
+# Requires SFTP credentials from Sanger
 sangerMutObj <- synGet("syn4633294", downloadLocation='/home/kdaily/data/Sanger/')
+
 system(sprintf("gunzip %s", getFileLocation(sangerMutObj)), 
        intern = FALSE, ignore.stdout = TRUE, ignore.stderr = TRUE, wait=TRUE)
-
 fileLoc <- gsub(".gz", "", getFileLocation(sangerMutObj))
+
 sangerMut <- fread(fileLoc, data.table=FALSE)
 
 sangerMutLINCS <- sangerMut %>% filter(`Sample name` %in% sangerLineNames)
